145108d73a3bea0f2fc0b558b1760db457bbcc81
jspaeth62 <60208438+jspaeth62@users.noreply.github.com>
2023-08-18 10:59:00 -0500

    8-18

---
+++ mlbread.py
@@ -0,0 +1,24 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+# URL of the webpage to scrape
+url = 'https://www.espn.com/mlb/scoreboard/_/date/20230818'
+
+# Send a GET request and retrieve HTML content
+response = requests.get(url)
+html_content = response.text
+
+# Parse HTML with BeautifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Example: Extract team names and scores from the webpage
+game_rows = soup.find_all('div', class_='scoreboard game-center align-items-center')
+for row in game_rows:
+    team_names = row.find_all('span', class_='sb-team-short')
+    team_scores = row.find_all('div', class_='total')
+    if len(team_names) == 2 and len(team_scores) == 2:
+        home_team_name = team_names[0].text
+        away_team_name = team_names[1].text
+        home_team_score = team_scores[0].text
+        away_team_score = team_scores[1].text
+        print(f"{home_team_name} {home_team_score} - {away_team_score} {away_team_name}")
---
+++ pickleread.py
@@ -0,0 +1,19 @@
+﻿import pickle
+import pandas as pd
+
+# Load the pickle file containing the dictionary
+with open('ncaafgames_dict.pickle', 'rb') as f:
+    data_dict = pickle.load(f)
+
+# Create an empty list to hold the dataframes
+dataframes = []
+
+# Iterate through the dictionary and append dataframes to the list
+for df in data_dict.values():
+    dataframes.append(df)
+
+# Concatenate the list of dataframes into a single dataframe
+merged_df = pd.concat(dataframes, ignore_index=True)
+
+# Save the merged dataframe to a CSV file
+merged_df.to_csv('merged_data.csv', index=False)
